425c548e2608bbcc0a66ce0c3d044c91dbe4e54a
eshuv <eshwar.vinnakota@menloschool.org>
2025-12-10 16:36:14 -0800

    transfered the helper functions from BSTNode class/file to BST.java. Also finished isValidBST!

--- src/BST.java
+++ src/BST.java
@@ -38,7 +38,7 @@ public class BST {
         for(int i=0; i<nodes.size()-1; i++) {
             System.out.print(nodes.get(i) + "-");
         }
-        System.out.println(nodes.get(nodes.size()-1));
+        System.out.println(nodes.getLast());
     }
 
     /**
@@ -47,39 +47,67 @@ public class BST {
      * @return true if val is in the tree, false otherwise
      */
     public boolean search(int val) {
-        // TODO: Complete the search function
-        if (root == null) return false;
-        return root.search(val);
+        return searchHelper(root, val);
+    }
+
+    private boolean searchHelper(BSTNode node, int val) {
+        if (node == null) return false;
+
+        if (val == node.getVal()) return true;
+        else if (val < node.getVal()) return searchHelper(node.getLeft(), val);
+        else return searchHelper(node.getRight(), val);
+    }
+
+
+
+    public void traverse(ArrayList<BSTNode> list, boolean l, boolean r) {
+        if (l && root.getLeft() != null) traverse(list, l, r);
+        if (r && root.getRight() != null) traverse(list, l, r);
+        list.add(this.getRoot());
+        if (!l && root.getLeft() != null) traverse(list, l, r);
+        if (!r && root.getRight() != null) traverse(list, l, r);
     }
 
     /**
      * @return ArrayList of BSTNodes in inorder
      */
+
     public ArrayList<BSTNode> getInorder() {
-        // TODO: Complete inorder traversal
         ArrayList<BSTNode> results = new ArrayList<>();
-        if (root != null) root.traverse(results,true, false);
+        inorderHelper(root, results);
         return results;
     }
 
-    /**
-     * @return ArrayList of BSTNodes in preorder
-     */
+    private void inorderHelper(BSTNode node, ArrayList<BSTNode> list) {
+        if (node == null) return;
+        inorderHelper(node.getLeft(), list);
+        list.add(node);
+        inorderHelper(node.getRight(), list);
+    }
+
     public ArrayList<BSTNode> getPreorder() {
-        // TODO: Complete preorder traversal
         ArrayList<BSTNode> results = new ArrayList<>();
-        if (root != null) root.traverse(results,false, false);
+        preorderHelper(root, results);
         return results;
     }
 
-    /**
-     * @return ArrayList of BSTNodes in postorder
-     */
+    private void preorderHelper(BSTNode node, ArrayList<BSTNode> list) {
+        if (node == null) return;
+        list.add(node);
+        preorderHelper(node.getLeft(), list);
+        preorderHelper(node.getRight(), list);
+    }
     public ArrayList<BSTNode> getPostorder() {
-        // TODO: Complete postorder traversal
         ArrayList<BSTNode> results = new ArrayList<>();
-        if (root != null) root.traverse(results,true, true);
+        postorderHelper(root, results);
         return results;
+    }
+
+    private void postorderHelper(BSTNode node, ArrayList<BSTNode> list) {
+        if (node == null) return;
+        postorderHelper(node.getLeft(), list);
+        postorderHelper(node.getRight(), list);
+        list.add(node);
     }
 
     /**
@@ -89,22 +117,32 @@ public class BST {
      * @param val The value ot insert
      */
     public void insert(int val) {
-        // TODO: Complete insert
-        if (root == null) {
-            root = new BSTNode(val);
-        } else {
-            root.insert(val);
-        }
+        root = insertHelper(root, val);
     }
 
-    /**
-     * Determines if the current BST is
-     * a valid BST.
-     * @return true if valid false otherwise
-     */
+    public BSTNode insertHelper(BSTNode node, int val) {
+        if (node == null) return new BSTNode(val);
+        if (val == node.getVal()) return node;
+        else if (val < node.getVal()) node.setLeft(insertHelper(node.getLeft(), val));
+        else node.setRight(insertHelper(node.getRight(), val));
+        return node;  // return the (possibly unchanged) subtree root
+    }
+
+
+        /**
+         * Determines if the current BST is
+         * a valid BST.
+         * @return true if valid false otherwise
+         */
     public boolean isValidBST() {
         // TODO: Optional Challenge!
-        return false;
+        return isValidBSTHelper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public boolean isValidBSTHelper(BSTNode node, int min, int max) {
+        if (node == null) return true;
+        if (node.getVal() <= min || node.getVal() >= max) return false;
+        return isValidBSTHelper(node.getLeft(), min, node.getVal()) && isValidBSTHelper(node.getRight(), node.getVal(), max);
     }
 
     public static void main(String[] args) {
@@ -134,5 +172,8 @@ public class BST {
         System.out.println("\nInorder traversal of binary tree is");
         sol = tree.getInorder();
         printNodes(sol);
+
+        System.out.println("Is valid BST? " + tree.isValidBST());
+
     }
 }
--- src/BSTNode.java
+++ src/BSTNode.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 
 /**
  * The Node for an Integer Binary Tree
@@ -10,15 +9,12 @@ import java.util.ArrayList;
 public class BSTNode {
     private BSTNode left;
     private BSTNode right;
-    private int val;
-    // depth is the largest possible number of generations below you
-    private int depth;
+    private final int val;
 
     public BSTNode(int val) {
         this.left = null;
         this.right = null;
         this.val = val;
-        this.depth = 0;
     }
 
     /** Getters and Setters **/
@@ -28,7 +24,6 @@ public class BSTNode {
 
     public void setLeft(BSTNode left) {
         this.left = left;
-        recomputeDepth();
     }
 
     public BSTNode getRight() {
@@ -38,49 +33,15 @@ public class BSTNode {
 
     public void setRight(BSTNode right) {
         this.right = right;
-        recomputeDepth();
     }
 
     public int getVal() {
         return val;
     }
 
-    private void recomputeDepth() {
-        depth = 0;
-        if (left != null && left.depth >= depth) depth = left.depth+1;
-        if (right != null && right.depth >= depth) depth = right.depth+1;
-    }
-
-    public boolean search(int value) {
-        // if value exists in tree return true, otherwise false
-        if (val == value) return true;
-        if (value < val && left != null) {
-            return left.search(value);
-        }
-        if (value > val && right != null) {
-            return right.search(value);
-        }
-        return false;
 
 
-    }
-    public void traverse(ArrayList<BSTNode> list, boolean l, boolean r) {
-        if (l && left != null) left.traverse(list,l,r);
-        if (r && right != null) right.traverse(list,l,r);
-        list.add(this);
-        if (!l && left != null) left.traverse(list,l,r);
-        if (!r && right != null) right.traverse(list,l,r);
-    }
 
-    public void insert(int value) {
-        if (val == value) return;
-        if (value < val && left != null) left.insert(value);
-        if (value < val && left == null) left = new BSTNode(value);
-        if (value > val && right != null) right.insert(value);
-        if (value > val && right == null) right = new BSTNode(value);
-        recomputeDepth();
-
-    }
 
     public String toString() {
         return Integer.toString(this.val);
